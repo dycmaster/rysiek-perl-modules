9eef249ca675fc063fb1d25e62bdb164537629aa
dycmaster <fryderyk.dyc@gmail.com>
2014-09-21 19:28:02 +0200

    user home wifi ssh creation moved

--- sensors/lib/Rysiek/Sensors/UserHomeWifi.pm
+++ sensors/lib/Rysiek/Sensors/UserHomeWifi.pm
@@ -9,24 +9,17 @@ package Rysiek::Sensors::UserHomeWifi v0.0.1{
 
   #needed! contains whole logic of constant measuring and
   #updating registered masters
-    sub constantMeasurements{
-      my $self = shift;
-      sleep(2);
+  sub constantMeasurements{
+    my $self = shift;
+    sleep(2);
 
-    my $cfg  = $self->sensorConfig();
-    my $host =  $cfg->[0]->{"router-hostname"};
-    my $user = $cfg->[0]->{"router-login"};
-    my $pass = $cfg->[0]->{"router-password"};
-    $self->{'ssh2'} = Net::OpenSSH->new("$user:$pass"."@".$host);
-    $self->{'ssh2'}->error and debug("Can't ssh to $host: " . $self->{'ssh2'}->error);
-    debug("SSH master client created");
 
-      while(1){
-        my $value = $self->measureOnce;
-        $self->updateMastersWithValue($value);
-        sleep $self->sensorConfig()->[0]->{"constantMeasureFrequency"};
-      }
+    while(1){
+      my $value = $self->measureOnce;
+      $self->updateMastersWithValue($value);
+      sleep $self->sensorConfig()->[0]->{"constantMeasureFrequency"};
     }
+  }
 
 
 #needed for a single-shot measurement
@@ -34,13 +27,23 @@ package Rysiek::Sensors::UserHomeWifi v0.0.1{
     my $self = shift;
     my $cfg  = $self->sensorConfig();
     my $cmd = "iw dev wlan0  station dump";
+
+    if(! defined $self->{'ssh2'}){
+      my $host =  $cfg->[0]->{"router-hostname"};
+      my $user = $cfg->[0]->{"router-login"};
+      my $pass = $cfg->[0]->{"router-password"};
+      $self->{'ssh2'} = Net::OpenSSH->new("$user:$pass"."@".$host);
+      $self->{'ssh2'}->error and debug("Can't ssh to $host: " . $self->{'ssh2'}->error);
+      debug("SSH master client created");
+    }
+
     my $stdout = $self->{'ssh2'}->capture($cmd);
 
     my @m = ( $stdout =~ /((?:[0-9a-f]{2}[:-]){5}[0-9a-f]{2})/ig );
     my $trackedMac = $cfg->[0]->{trackedMac};
     debug("MACs in UserHomeWifi:");
     debug("@m");
-    
+
     if ( $trackedMac ~~ @m) {
       debug("true");
       return "1";
@@ -48,7 +51,7 @@ package Rysiek::Sensors::UserHomeWifi v0.0.1{
       debug("false");
       return "0";
     }   
-    
+
   }
 
   true;
